5e2271085075264795a53e769abae401ab250ff3
amandaolits <amandaolits@gmail.com>
2025-03-30 17:04:45 -0300

    feat: adjust screen size

--- src/styles/DashboardStyles.ts
+++ src/styles/DashboardStyles.ts
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const DashboardContainer = styled.div`
   max-width: 1200px;
   margin: 0 auto;
-  padding: 20px;
 `;
 
 export const SearchContainer = styled.div`
