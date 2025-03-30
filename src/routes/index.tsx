da91ce090c2cf755762ef46fd7ae571f6a66c670
amandaolits <amandaolits@gmail.com>
2025-03-30 15:32:13 -0300

    feat: fix routes and add new page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,17 @@ import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 import DashboardPage from '../pages/DashboardPage';
 import AddressPage from '../pages/AddressPage';
+import StatementPage from '../pages/StatementPage';
 
 const RoutesConfig = () => (
   <UserProvider>
     <Router>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/address" element={<AddressPage />} />
+        <Route path="/statement" element={<StatementPage />} />
       </Routes>
     </Router>
   </UserProvider>
